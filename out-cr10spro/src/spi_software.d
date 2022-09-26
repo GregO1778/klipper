2cba81e28e346db51dcc4c2b656067e146ec0eb7
Greg Oliver Jr <GregO1778@gmail.com>
2022-09-26 09:18:32 -0400

    Adding CR-10s Pro firmware build

---
+++ out-cr10spro/src/spi_software.d
@@ -0,0 +1,5 @@
+out/src/spi_software.o: src/spi_software.c out/board/gpio.h \
+ /usr/lib/gcc/avr/5.4.0/include/stdint.h /usr/lib/avr/include/stdint.h \
+ src/basecmd.h /usr/lib/gcc/avr/5.4.0/include/stddef.h src/command.h \
+ /usr/lib/gcc/avr/5.4.0/include/stdarg.h src/ctr.h src/compiler.h \
+ src/sched.h src/spicmds.h
